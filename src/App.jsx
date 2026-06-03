bfee80fd60761c873bc1ac8253b8d4336bcd8a96
Kowalczuk <wkowalczuk03@gmail.com>
2026-06-03 12:15:47 +0200

    first steps in react

--- src/App.jsx
+++ src/App.jsx
@@ -5,118 +5,63 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+  const [registered, setRegistered] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(""); // do kogo wyslac
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started HAHAHA </h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+  if(!registered){
+    return (
+    <><div>
+      <h2>Join Chat</h2>
+      <input id="name"
+          type="text"
+          placeholder="Your name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <button id="connect_btn"
+        type="button"
+        
+        onClick={() => setRegistered((registered) => registered = true)}
         >
-          Count is {count}
+          Connect
         </button>
-      </section>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    </div>
     </>
-  )
+  );
+
+  }else{
+    return(
+    <><div>
+      <h2>Chat</h2>
+      <textarea id="message"
+          type="text"
+          placeholder="Your message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        >
+        </textarea>
+        <button id="send_btn" onClick={() => {
+          setMessages([...messages, `${name}: ${message}`]);
+          setMessage(""); // clear input
+        }}>
+          Send
+        </button>
+
+    </div>
+    <div id="messages_div">
+          {messages.map((msg, i) => (<p key={i}>{msg}</p>))}
+    </div>
+    </>
+    );
+  }
+
+  
 }
 
-export default App
+export default App;
